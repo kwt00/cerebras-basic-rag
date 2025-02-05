9fdfc306a44cbc5df271c85ab58e7c287c10b9f4
kwt00 <116334641+kwt00@users.noreply.github.com>
2025-02-05 15:10:14 -0800

    Update generator.py

--- generator.py
+++ generator.py
@@ -3,7 +3,7 @@ from cerebras.cloud.sdk import Cerebras
 
 class Generator:
     def generate(self, input_data, prompt):
-        client = Cerebras(api_key="csk-mk4cw585dcf8p263hcyj68hnyw242cfr9dynhr3xpmjfrkcc")
+        client = Cerebras(api_key="")
         chat_completion = client.chat.completions.create(
             messages=[{"role": "user", "content": prompt+"USE THE FOLLOWING AS RAG TO ANSWER THE PROMPT (USER CANT SEE): "+str(input_data)}],
             model="llama3.3-70b",
